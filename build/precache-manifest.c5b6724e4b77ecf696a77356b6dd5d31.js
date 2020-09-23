78bc8a86105c23aee2f5699794037e0c8db1beb6
annu_singh <annusingh2403@gmail.com>
2020-09-23 17:07:37 +0530

    i

---
+++ build/precache-manifest.c5b6724e4b77ecf696a77356b6dd5d31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcb2238ad60226b861fc8838f424667e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2c7503132e6d1dc6170",
+    "url": "/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "62d1a7d1defcafb445fb",
+    "url": "/static/css/main.a0913315.chunk.css"
+  },
+  {
+    "revision": "b2c7503132e6d1dc6170",
+    "url": "/static/js/2.c4103220.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c4103220.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62d1a7d1defcafb445fb",
+    "url": "/static/js/main.d3290352.chunk.js"
+  },
+  {
+    "revision": "b957cec06c6437b7ad73",
+    "url": "/static/js/runtime-main.c7ceb169.js"
+  }
+]);
